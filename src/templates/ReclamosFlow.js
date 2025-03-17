b45f1628ed4517ada26096b55bb0850417f57c77
BotCuenta <corrientesdefensoria@gmail.com>
2025-03-17 19:37:54 -0300

    Update ReclamosFlow.js

--- src/templates/ReclamosFlow.js
+++ src/templates/ReclamosFlow.js
@@ -2,22 +2,26 @@ import { addKeyword, EVENTS } from "@builderbot/bot";
 import { insertInSheets } from "../config/sheets.js";
 
 export const reclamosFlow = addKeyword(EVENTS.ACTION)
-	.addAction({ capture: true },async (ctx, { flowDynamic }) => {
-		await flowDynamic(`Escribe más detalles sobre tu reclamo.`);
-	})
-	.addAction({ capture: true }, async (ctx, { flowDynamic, state }) => {
-		const { nombreCompleto, documento, area } = state.getMyState();
-		const response = await insertInSheets(
-			{
-				nombreCompleto,
-				documento,
-				area,
-				telefono: ctx.from,
-				motivo: ctx.body,
-			},
-			"RECLAMOS"
-		);
-		await flowDynamic([
-			{ body: response, buttons: [{ body: "Volver al Inicio" }] },
-		]);
-	});
+    .addAction(async (ctx, { flowDynamic }) => {
+        await flowDynamic(`Escribe más detalles sobre tu reclamo.`);
+    })
+    .addAction({ capture: true }, async (ctx, { flowDynamic, state, endFlow }) => {
+        const { nombreCompleto, documento, area } = state.getMyState();
+        const response = await insertInSheets(
+            {
+                nombreCompleto,
+                documento,
+                area,
+                telefono: ctx.from,
+                motivo: ctx.body,
+            },
+            "RECLAMOS"
+        );
+
+        await flowDynamic([
+            { body: response, buttons: [{ body: "Volver al Inicio" }] },
+        ]);
+
+        // Finaliza el flujo para evitar que se vuelva a Dialogflow
+        return endFlow();
+    });
