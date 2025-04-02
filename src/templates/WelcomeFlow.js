75e7d43f5a6e7d122962cad7f3196b32f131b757
Augusto <augustojedamathe@gmail.com>
2025-04-02 17:38:07 -0300

    consultas 2
    Co-authored-by: Ariel Alejandro Ayala <arielAayala@users.noreply.github.com>

--- src/templates/WelcomeFlow.js
+++ src/templates/WelcomeFlow.js
@@ -32,7 +32,7 @@ const welcomeFlow = addKeyword(EVENTS.WELCOME).addAction(async (ctx, ctxFn) => {
     response.queryResult.intent.displayName.includes("¿") || response.queryResult.intent.displayName.includes("?")){
 		let nombreCompleto = response.queryResult.parameters?.nombreCompleto;
 		let documento = response.queryResult.parameters?.documento;
-		let area = response.queryResult.parameters?.areasEntity;
+		let area = response.queryResult.parameters?.areas.stringValue;
 		let consulta = response.queryResult.intent.displayName
 		console.log({parametros: response.queryResult.parameters})
 		if (!nombreCompleto || !documento) {
