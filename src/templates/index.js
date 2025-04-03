eab1253f23a611388f351f1b768697b7431386a2
BotCuenta <corrientesdefensoria@gmail.com>
2025-04-03 00:51:25 -0300

    Update index.js

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,26 +1,26 @@
 import { createFlow } from "@builderbot/bot";
 import { conversacionalFlow, endFlow, welcomeFlow } from "./WelcomeFlow.js";
-import { reclamosFlow } from "./ReclamosFlow.js";
-import {consultasFlow} from "./ConsultasFlow.js"
-import { sugerenciasFlow,detalleSugerenciasFlow,subirSugerenciasFlow } from "./SugerenciasFlow.js";
-import {
-	audioFlow,
-	locationFlow,
-	mediaFlow,
-	documentFlow,
-} from "./MediaFlow.js";
+import { reclamosFlow, pedirAreaFlow, pedirMotivoFlow, pedirNombreFlow, pedirDocumentoFlow, guardarReclamoFlow } from "./ReclamosFlow.js";
+import { consultasFlow } from "./ConsultasFlow.js";
+import { sugerenciasFlow, detalleSugerenciasFlow, subirSugerenciasFlow } from "./SugerenciasFlow.js";
+import { audioFlow, locationFlow, mediaFlow, documentFlow } from "./MediaFlow.js";
 
 export default createFlow([
-	welcomeFlow,
-	conversacionalFlow,
-	endFlow,
-	reclamosFlow,
-	audioFlow,
-	locationFlow,
-	mediaFlow,
-	documentFlow,
-	consultasFlow,
-	sugerenciasFlow,
-	detalleSugerenciasFlow,
-	subirSugerenciasFlow
+    welcomeFlow,
+    conversacionalFlow,
+    endFlow,
+    reclamosFlow,
+    pedirAreaFlow,
+    pedirMotivoFlow,
+    pedirNombreFlow,
+    pedirDocumentoFlow,
+    guardarReclamoFlow,
+    audioFlow,
+    locationFlow,
+    mediaFlow,
+    documentFlow,
+    consultasFlow,
+    sugerenciasFlow,
+    detalleSugerenciasFlow,
+    subirSugerenciasFlow
 ]);
